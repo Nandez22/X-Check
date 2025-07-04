6920a6c6250b7f79d15ac0ca5024dd3787900889
JHernandez <joseph.hernandez7479@gmail.com>
2025-07-03 21:26:32 -0500

    Message System Rework

--- panel/panel.js
+++ panel/panel.js
@@ -20,8 +20,7 @@ function sendEvalRequest(xPath){
 function sendQuery(data){
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { data }, (response) => {
-            console.log("Response from content script:", response)
-            displayResults(response);
+            handleResponse(response)
         });
     });
 }
@@ -43,7 +42,6 @@ function createResultContainer(result, doc, resultsContainer) {
     resultsContainer.appendChild(container);
 }
 
-
 function displayResults(results) {
     const iframe = document.getElementById('results');
     const doc = iframe.contentDocument || iframe.contentWindow.document;
@@ -55,4 +53,41 @@ function displayResults(results) {
     results.forEach(result => {
         createResultContainer(result, doc, resultsContainer);
     });
+}
+
+function handleResponse(response) {
+    if(!response || !response.type) return;
+
+    switch(response.type) {
+        case XPathResult.STRING_TYPE:
+            console.log(response.data)
+            break;
+
+        case XPathResult.NUMBER_TYPE:
+            console.log(response.data)
+            break;
+
+        case XPathResult.BOOLEAN_TYPE:
+            console.log(response.data)
+            break;
+
+        case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
+        case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
+            console.log(response.data)
+            break;
+        
+        case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
+        case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
+            console.log(response.data)
+            break;
+
+        case XPathResult.ANY_UNORDERED_NODE_TYPE:
+        case XPathResult.FIRST_ORDERED_NODE_TYPE:
+            console.log(response.data)
+            break;
+
+        default:
+            console.warn("Unhandled XPathResult Type: ", results.resultType);
+            break;
+    }
 }
--- scripts/xc-Agent.js
+++ scripts/xc-Agent.js
@@ -2,62 +2,164 @@
 console.log("xCheckAgent.js loaded");
 injectStylesheet('resources/xc-styles.css');
 
-
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.data.type === "evalXPath") {
         removeHighlights();
 
         let results = evalXPath(message.data.xPath)
-        highlightResults(results);
-        sendResponse(packageResults(results));
+        let resultsPackage = handleResults(results);
+
+        sendResponse(resultsPackage);
     }
 });
 
-function packageResults(elements) {
-    if (!elements || elements.length === 0) {
-        return [];
+//TODO assign UID to each node for individual selections (will be reset and cleared per-search)
+//This does not apply to anything other than ELEMENT_NODEs (since nothing else can be highlighted)
+function serializeNode(node) {
+    let data;
+    switch(node.nodeType){
+        case(Node.ELEMENT_NODE):
+            data = {
+                tagName: node.tagName,
+                attributes: Array.from(node.attributes).map(a => ({
+                    name: a.name,
+                    value: a.value
+                }))
+            }
+            break;
+
+        case(Node.TEXT_NODE):
+        case(Node.COMMENT_NODE):
+            data = {
+                text: node.textContent.trim()
+            }
+            break;
+
+        case(Node.ATTRIBUTE_NODE):
+            data = {
+                name: node.name,
+                value: node.value
+            }
+            break;
+
+        case(Node.PROCESSING_INSTRUCTION_NODE):
+            data = {
+                target: node.target,
+                data: node.data
+            }
+            break;
+
+        default:
+            break;
     }
 
-    return elements.map(element => {
-        return {
-            tagName: element.tagName,
-        };
-    });
-}
-
-function evalXPath(xPath){
-    try{
-        let results = document.evaluate(xPath, document, null, XPathResult.ANY_TYPE)
-        let elements = [];
-
-        let result = results.iterateNext();
-        while (result) {
-            elements.push(result);
-            result = results.iterateNext();
-        }
-
-        return elements;
-
-    } catch (error) {
-        return [];
+    return {
+        nodeType: node.nodeType,
+        data: data
     }
 }
 
-function highlightResults(results) {
-    if (!results || results.length === 0) { return; }
-    results.forEach(element => {
-        element.classList.add('xc-highlight');
-    });
+function packageItterators(results) {
+    const nodes = [];
+    let node;
+
+    while((node = results.iterateNext())) {
+        nodes.push(node);
+    }
+
+    const serialized = nodes.map(node => serializeNode(node))
+    nodes.forEach(highlightElement);
+
+    return serialized;
+}
+
+function packageSnapshots(results) {
+    const nodes = [];
+
+    for(let i = 0; i < results.snapshotLength; i++){
+        nodes.push(results.snapshotItem(i));
+    }
+
+    const serialized = nodes.map(node => serializeNode(node))
+    nodes.forEach(highlightElement);
+
+    return serialized;
+}
+
+
+function evalXPath(xPath) {
+    try {
+        return document.evaluate(xPath, document, null, XPathResult.ANY_TYPE, null);
+    } catch {
+        return null;
+    }
+}
+
+//We want to process different types of results
+//For elements we want to highlight
+//For expressions with bool, number or text responses we want to display them properly
+function handleResults(results) {
+    if(!results) return { type: null, data: null };
+    let data = null
+
+    switch(results.resultType) {
+        case XPathResult.STRING_TYPE:
+            data = results.stringValue;
+            break;
+
+        case XPathResult.NUMBER_TYPE:
+            data = results.numberValue;
+            break;
+
+        case XPathResult.BOOLEAN_TYPE:
+            data = results.booleanValue;
+            break;
+
+        case XPathResult.ORDERED_NODE_ITERATOR_TYPE:
+        case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
+            data = packageItterators(results);
+            break;
+        
+        case XPathResult.ORDERED_NODE_SNAPSHOT_TYPE:
+        case XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE:
+            data = packageSnapshots(results);
+            break;
+
+        case XPathResult.ANY_UNORDERED_NODE_TYPE:
+        case XPathResult.FIRST_ORDERED_NODE_TYPE:
+            data = results.singleNodeValue;
+            break;
+
+        default:
+            console.warn("Unhandled XPathResult Type: ", results.resultType);
+            break;
+    }
+
+    resultPackage = {
+        type: results.resultType,
+        data: data
+    }
+    console.log(`${resultPackage.type} : ${resultPackage.data}`)
+    return resultPackage;
+}
+
+
+function highlightElement(element) {
+    if(!element || element.nodeType != Node.ELEMENT_NODE) return;
+    element.classList.add('xc-highlight');
+}
+
+function removeHighlight(element) {
+    if(!element) return;
+    element.classList.remove('xc-highlight');
 }
 
 function removeHighlights() {
     let highlightedElements = document.querySelectorAll('.xc-highlight');
     highlightedElements.forEach(element => {
-        element.classList.remove('xc-highlight');
+        removeHighlight(element);
     });
 }
-
 
 function injectStylesheet(path) {
     const link = document.createElement('link');
